fe9e329d32372655f0515d330a29fe5fe2e30ab2
Anton <rudnev.exceedteam@gmail.com>
2022-02-10 09:13:58 +0300

    added tests

--- 1. Basic/3) basic-calc/index.js
+++ 1. Basic/3) basic-calc/index.js
@@ -1,6 +1,22 @@
 function basicCalc(operation, a, b) {
-  // Напиши свой код здесь
+  switch (operation) {
+      case '+':
+          return a + b;
+      case '-':
+          return a - b;
+      case '/':
+          return a / b;
+      case '*':
+          return a * b;
+      default:
+          console.log("Неверная операция");
+    }
 }
+
+console.log(basicCalc('+', 4, 5));
+console.log(basicCalc('-', 4, 5));
+console.log(basicCalc('/', 7, 5));
+console.log(basicCalc('*', 78, 34));
 
 window.basicCalc = basicCalc;
 
--- 1. Basic/4) factorial/index.js
+++ 1. Basic/4) factorial/index.js
@@ -1,6 +1,17 @@
 function factorial(n) {
-  // Напиши свой код здесь
+    if((n > 0) && (n <= 10)) {
+        let count = 1;
+        for (let i = 2; i <= n; i++) {
+            count *= i;
+        }
+        return count;
+    }
+    else {
+        console.log("Неверное значение n");
+    }
 }
+
+console.log(factorial(7))
 
 window.factorial = factorial;
 
