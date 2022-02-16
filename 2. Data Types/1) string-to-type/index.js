72a32a3a951fafcb0df7343ec6625a73a5005136
Anton <rudnev.exceedteam@gmail.com>
2022-02-16 12:54:42 +0300

    Finished string to type

--- 2. Data Types/1) string-to-type/index.js
+++ 2. Data Types/1) string-to-type/index.js
@@ -1,7 +1,29 @@
 function stringToType(str) {
-  // Напиши свой код здесь
-};
+    if (str === "null") {
+        return null;
+    }
+    if (str === "undefined") {
+        return undefined;
+    }
+    if (str === "true") {
+        return true;
+    }
+    if (str === "false") {
+        return false
+    }
+    if (str === "NaN") {
+        return NaN;
+    }
+    if (str === "") {
+        return str;
+    }
+    if (str === Number(str)) {
+        return Number(str);
+    }
+        return str;
+}
 
 window.stringToType = stringToType;
 
 export default stringToType;
+
