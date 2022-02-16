260001ffc00ea5f53f00fc4b24a06dfa14100c26
Anton <rudnev.exceedteam@gmail.com>
2022-02-16 14:34:31 +0300

    Finished full-sum

--- 3. Array/1) full-sum/index.js
+++ 3. Array/1) full-sum/index.js
@@ -1,5 +1,16 @@
-function fullSum(...args) {
-  // Напиши свой код здесь
+function fullSum(args) {
+
+    let sum = 0;
+
+    for (let i = 0; i < arguments.length; i++) {
+        if (typeof arguments[i] !== 'number') {
+            throw new TypeError("Wrong Argument Type");
+        }
+        else {
+            sum += arguments[i];
+        }
+    }
+    return sum;
 }
 
 window.fullSum = fullSum;
