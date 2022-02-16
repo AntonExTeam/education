f6b9d923acf94de632ae937d3195a28951d4080c
Anton <rudnev.exceedteam@gmail.com>
2022-02-16 16:21:20 +0300

    Modified sum-check

--- 2. Data Types/2) sum-check/index.js
+++ 2. Data Types/2) sum-check/index.js
@@ -1,10 +1,9 @@
 function sumCheck(a, b) {
-    if ((typeof a === "number") && (typeof b === "number")){
-        return a + b;
-    }
-    else {
+    if ((typeof a !== "number") || (typeof b !== "number")){
         throw new TypeError("Wrong arguments type!");
+
     }
+    return a + b;
 }
 
 window.sumCheck = sumCheck;
