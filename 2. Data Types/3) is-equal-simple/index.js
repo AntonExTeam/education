d5de6f3b742336c770551c2065360db0681bc7df
Anton <rudnev.exceedteam@gmail.com>
2022-02-16 14:32:09 +0300

    Finished is-equal-simple

--- 2. Data Types/3) is-equal-simple/index.js
+++ 2. Data Types/3) is-equal-simple/index.js
@@ -1,6 +1,36 @@
 function isEqual(a, b) {
-  // Напиши свой код здесь
+
+    if(a === b) {
+        return true
+    }
+
+    if (a == null && b != null) {
+        return false;
+    }
+
+    if (b == null && a != null) {
+        return false;
+    }
+
+    let keys1 = Object.keys(a);
+    let keys2 = Object.keys(b);
+
+    if (keys1.length !== keys2.length) {
+        return false;
+    }
+
+    for (let key of keys1) {
+        if(!b.hasOwnProperty(key)) {
+            return false;
+        }
+        if (a[key] !== b[key]) {
+            return false;
+        }
+
+    }
+    return true;
 }
+
 
 window.isEqual = isEqual;
 
