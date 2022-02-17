76fb7f988c49d01fb39d1c106c5aaf8b7be9286b
Anton <rudnev.exceedteam@gmail.com>
2022-02-17 09:45:57 +0300

    Finished query-object

--- 3. Array/4) query-object/index.js
+++ 3. Array/4) query-object/index.js
@@ -1,5 +1,19 @@
+import {stringToType} from "../../2. Data Types/1) string-to-type/index.js"
+
 function queryToObject(query) {
-  // Напиши свой код здесь
+
+    let searchParams = new URLSearchParams(query);
+    let arrParams = searchParams.entries();
+
+    function paramsToObject(entries) {
+        let result = {}
+        for (let [key, value] of entries) {
+            result[key] = stringToType(value);
+        }
+        return result;
+    }
+
+    return paramsToObject(arrParams);
 };
 
 window.queryToObject = queryToObject;
