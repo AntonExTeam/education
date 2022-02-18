282a2a35297a4465c2fc8ace12345ed5a13c314e
Anton <rudnev.exceedteam@gmail.com>
2022-02-18 14:23:14 +0300

    Modified async-sum

--- 4. Async/1) async-sum/index.js
+++ 4. Async/1) async-sum/index.js
@@ -1,14 +1,12 @@
-function asyncSum(a, b) {
+async function asyncSum(a, b) {
 
-    return new Promise((resolve, reject) => {
-        if ((typeof a === 'number') && (typeof b === 'number')) {
-            setTimeout(() => resolve(a + b), 1000);
-        }
-        else {
+    return await new Promise((resolve, reject) => {
+        if ((typeof a !== 'number') || (typeof b !== 'number')) {
             reject(new Error());
+
         }
-    }).then(result => result)
-      .then(err => err);
+        return setTimeout(() => resolve(a + b), 1000);
+    })
 }
 
 window.asyncSum = asyncSum;
