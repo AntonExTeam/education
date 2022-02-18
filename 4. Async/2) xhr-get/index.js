f5b53c346074a2d19f612bf94b51c2289ee9990c
Anton <rudnev.exceedteam@gmail.com>
2022-02-18 16:27:56 +0300

    Finished xhr-get

--- 4. Async/2) xhr-get/index.js
+++ 4. Async/2) xhr-get/index.js
@@ -1,5 +1,26 @@
 function xhrGet(url) {
-  // Напиши свой код здесь
+
+    let xhr = new XMLHttpRequest();
+
+    xhr.open('GET', url);
+
+    xhr.responseType = 'json';
+
+    xhr.setRequestHeader('Content-Type', 'application/json');
+
+    xhr.send();
+
+    return new Promise((resolve, reject) => {
+        xhr.onload = () => {
+            if (xhr.status >= 400) {
+                reject(xhr.response);
+            }
+            resolve(xhr.response);
+        }
+        xhr.onerror = () => {
+            reject(xhr.response);
+        };
+    });
 }
 
 window.xhrGet = xhrGet;
