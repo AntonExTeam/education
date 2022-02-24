5e27e889f6d95651e6126eb7b54a2e8e6f44ae31
Anton <rudnev.exceedteam@gmail.com>
2022-02-24 17:11:08 +0300

    Finished row-zero

--- 5. Others/6) row-zero/index.js
+++ 5. Others/6) row-zero/index.js
@@ -1,5 +1,17 @@
 function rowZero(str) {
-  // Напиши свой код здесь
+
+    let numOfZero = str.match(/0+/g);
+    let zeroCount = [];
+
+    if ((str === "") || (str.indexOf("0") === -1)) {
+        return 0;
+    }
+
+    for (let i = 0; i < numOfZero.length; i++) {
+        zeroCount.push(numOfZero[i].length);
+    }
+
+    return Math.max(...zeroCount);
 }
 
 window.rowZero = rowZero;
