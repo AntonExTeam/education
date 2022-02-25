1bb3b5d71cc56e046e8a36f5fcc1f5b2059f39dd
Anton <rudnev.exceedteam@gmail.com>
2022-02-25 10:04:17 +0300

    Finished is-equal-advances

--- 5. Others/7) is-equal-advances/index.js
+++ 5. Others/7) is-equal-advances/index.js
@@ -1,5 +1,34 @@
+const {isEqual} = require("../../2. Data Types/3) is-equal-simple/index")
+
 function isEqualAdvances(a, b) {
-  // Напиши свой код здесь
+
+    if (a === b) {
+        return true
+    }
+
+    if (a === null || b === null) {
+        return false;
+    }
+
+    let keys1 = Object.keys(a);
+    let keys2 = Object.keys(b);
+
+    if (keys1.length !== keys2.length) {
+        return false;
+    }
+
+    for (let key of keys1) {
+        if ((typeof a[key] === 'object') && (typeof b[key] === 'object')) {
+            return isEqual(a[key], b[key])
+        }
+        if (!b.hasOwnProperty(key)) {
+            return false;
+        }
+        if (a[key] !== b[key]) {
+            return false;
+        }
+    }
+    return true;
 }
 
 window.isEqualAdvances = isEqualAdvances;
